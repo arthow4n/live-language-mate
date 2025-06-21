99892946750a9510f61dc4b819951561542e76be
Sheng-Han (Aysh) Su <13860646+arthow4n@users.noreply.github.com>
2025-06-21 03:37:55 +0200

    Skip lint rules that don't work with Deno

--- eslint.config.js
+++ eslint.config.js
@@ -3,34 +3,46 @@ import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import tseslint from 'typescript-eslint';
 import prettierConfig from 'eslint-config-prettier';
+import path from 'node:path';
 
 /** @type {import("eslint").Linter.Config[]} */
 export default tseslint.config(
   { ignores: ['dist'] },
   js.configs.recommended,
-  tseslint.configs.strictTypeChecked,
-  tseslint.configs.stylisticTypeChecked,
   prettierConfig,
   {
+    extends: [
+      tseslint.configs.strictTypeChecked,
+      tseslint.configs.stylisticTypeChecked,
+    ],
+    files: ['**/*.{ts,tsx}'],
+    plugins: {
+      'react-hooks': reactHooks,
+    },
     languageOptions: {
       globals: {
         ...globals.browser,
       },
       parserOptions: {
-        projectService: {
-          allowDefaultProject: ['api/*.{ts,tsx}', 'api/handlers/*.{ts,tsx}'],
-        },
+        projectService: true,
         tsconfigRootDir: import.meta.dirname,
       },
     },
-  },
-  {
-    files: ['**/*.{ts,tsx}'],
-    plugins: {
-      'react-hooks': reactHooks,
-    },
     rules: {
       ...reactHooks.configs.recommended.rules,
+    },
+  },
+  {
+    // For Deno files, turn off rules which need type information, I don't know how to config this properly yet.
+    files: ['api/**/*.ts'],
+    extends: [tseslint.configs.strict, tseslint.configs.stylistic],
+    languageOptions: {
+      // globals: {
+      //   ...globals.browser,
+      // },
+      parserOptions: {
+        tsconfigRootDir: path.join(import.meta.dirname, 'api'),
+      },
     },
   }
 );
