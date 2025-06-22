196cbfe0603e2159e71470d33d172599f2ec15ef
Sheng-Han (Aysh) Su <13860646+arthow4n@users.noreply.github.com>
2025-06-22 15:01:34 +0200

    Reduce test context noise for LLM

--- vitest.config.ts
+++ vitest.config.ts
@@ -11,6 +11,19 @@ export default defineConfig({
   },
   test: {
     environment: 'jsdom',
+    onStackTrace(error, { file }) {
+      // Reduce context noise for LLM.
+
+      // If we've encountered a ReferenceError, show the whole stack.
+      if (error.name === 'ReferenceError') {
+        return;
+      }
+
+      // Reject all frames from third party libraries.
+      if (file.includes('node_modules')) {
+        return false;
+      }
+    },
     setupFiles: ['./src/__tests__/setup.ts'],
     typecheck: {
       enabled: true,
